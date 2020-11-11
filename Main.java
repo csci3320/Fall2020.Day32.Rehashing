aa399d5905bba93d56ab3703fd2ae7a4a9c3435e
UNO User <unouser@unomaha.edu>
2020-11-11 12:46:27 -0600

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    new Main();
+  }
+
+ 
+  public Main() {
+    HashTable<String> hashTable = new HashTable<String>();
+    
+    String[] strings = { "Happy", "Veterans", "Day", "computer", "science", "csci", "3320", "data", "structures" };
+    for (int i = 0; i < strings.length; i++) {
+      String string = strings[i];
+      hashTable.add(string);
+    }
+
+    hashTable.print();
+
+    System.out.println(hashTable.contains("Happy"));
+    System.out.println(hashTable.contains("Sad"));
+  }
+
+  
+}
